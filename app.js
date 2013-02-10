ae838b7e4ec0e33a66f5e7f8ae8ee68225b1d386
Andrew Lovett Barron <andrewlb@Andrews-MacBook-Pro.local>
2013-02-10 13:29:56 -0500

    Storyboarding?

--- app.js
+++ app.js
@@ -13,7 +13,33 @@ var express = require('express')
 
 var app = express();
 
-mongoose.connect("mongodb://<user>:<password>@linus.mongohq.com:10084/calc_test")
+mongoose.connect("mongodb://" + config.user + ":" + config.pass + "@linus.mongohq.com:10084/calc_test")
+
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+
+var userSchema = new Schema({
+    userid    : ObjectId
+  , name      : String
+  , date      : Date
+});
+
+var pairSchema = new Schema({
+    users         : [Schema.Types.ObjectId]
+    , difference  : Number
+    , owe         : ObjectId
+});
+
+var itemSchema = new Schema({
+    user     : ObjectId
+  , date     : Date
+  , amount   : String
+  , type     : String
+});
+
+var userModel = mongoose.model('user', userSchema, 'users');
+var pairModel = mongoose.model('pair', pairSchema, 'pairs');
+var itemModel = mongoose.model('item', itemSchema, 'items');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -31,8 +57,34 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/user/:userId', function(req,res) {
+
+});
+
+app.post('/user', function(req,res) {
+  // Make an account 
+
+});
+
+app.get('/list/:userid/:page', function(req,res) {
+  var instance = new userModel();
+  instance.my.key = 'hello';
+  instance.save(function (err) {
+    //
+  });
+});
+
+app.post('/add/:userid/:amount/:type/:date', function(req,res) {
+
+
+});
+
+
+app.get('/difference/:userid/:difference', function(req,res) {
+
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
